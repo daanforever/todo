63d5b19b0e12c909394e26ab98f83f21601ca470
dan <daan.forever@gmail.com>
2012-12-12 15:37:39 +0400

    thin support in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,11 +20,20 @@ set :user, "dan"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  desc "Start the Thin processes"
+  task :start do
+    run "cd #{deploy_to}/current && bundle exec thin start -C config/thin.yml"
+  end
+
+  desc "Stop the Thin processes"
+  task :stop do
+    run "cd #{deploy_to}/current && bundle exec thin stop -C config/thin.yml"
+  end
+
+  desc "Restart the Thin processes"
+  task :restart do
+    run "cd #{deploy_to}/current && bundle exec thin restart -C config/thin.yml"
+  end
+
+end
